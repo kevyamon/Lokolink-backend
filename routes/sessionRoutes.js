05689e3a85d9da366985c7997d99642df053a49b
baticlean <baticlean225@gmail.com>
2025-11-15 13:50:39 +0000

    inscription

--- routes/sessionRoutes.js
+++ routes/sessionRoutes.js
@@ -4,25 +4,28 @@ const express = require('express');
 const router = express.Router();
 const {
   createSession,
+  getMySession, // Import nouveau
   deleteSession,
   getActiveSessions,
   getSessionDetails,
+  joinSession, // Import nouveau
 } = require('../controllers/sessionController');
 
-// Importer notre "garde" JWT
 const { protect } = require('../middleware/authMiddleware');
 
 // --- Routes Protégées (Délégué/Admin) ---
 
 // POST /api/sessions/create
-// 'protect' s'exécute en premier. S'il passe, il appelle 'createSession'.
 router.post('/create', protect, createSession);
+
+// GET /api/sessions/my-session/:id (POUR LE DASHBOARD DÉLÉGUÉ)
+router.get('/my-session/:id', protect, getMySession);
 
 // DELETE /api/sessions/:id
 router.delete('/:id', protect, deleteSession);
 
 
-// --- Routes Publiques (Filleul) ---
+// --- Routes Publiques (Filleul & Parrains) ---
 
 // GET /api/sessions/active
 router.get('/active', getActiveSessions);
@@ -30,5 +33,7 @@ router.get('/active', getActiveSessions);
 // GET /api/sessions/:id
 router.get('/:id', getSessionDetails);
 
+// POST /api/sessions/join (Inscription Parrain via Lien)
+router.post('/join', joinSession);
 
 module.exports = router;
